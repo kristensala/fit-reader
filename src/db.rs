491b91551309294d75f76fa9e60c6fb64a8775d9
Kristen Sala <salakristen1@gmail.com>
2023-01-12 00:08:31 +0200

    init db and restructure some files

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,37 @@
 use rusqlite::{Connection, Result as SqliteResult};
 
-fn connect() {
+use crate::parser::Session;
+
+fn open_connection() -> SqliteResult<Connection> {
+    return Ok(Connection::open("test.db")?);
 }
 
-fn save() {
+pub fn init() -> SqliteResult<()> {
+    let connection = open_connection()?;
 
+    connection.execute(
+        "create table if not exists session (
+            id integer primary key,
+            name text not null
+        )",
+        []
+    )?;
+
+    return Ok(());
 }
+
+pub fn insert_session(session: Session) -> SqliteResult<()> {
+    let connection = open_connection()?;
+
+
+    return Ok(());
+}
+
+pub fn get_session_by_id(id: i32) -> SqliteResult<()> { // should return session
+    return Ok(()); 
+}
+
+pub fn get_sessions_by_date_range(start: f64, end: f64) -> SqliteResult<()> {
+    return Ok(());
+}
+
